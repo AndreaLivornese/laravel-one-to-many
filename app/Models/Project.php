2bd7232e425ca9ce9c5577078c508af950babc23
andrealivornese <andrealivornese@gmail.com>
2024-04-23 15:40:20 +0200

    create le relazioni tra i models Project e Type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,5 +10,9 @@ class Project extends Model
 {
     use HasFactory;
 
-    // protected $fillable =['name', 'username_creator','link_github','program_langs','img'];
+    protected $fillable =['name', 'username_creator','link_github','program_langs','img'];
+
+    public function type(){
+        return $this->belongTo(Type::class);
+    }
 }
